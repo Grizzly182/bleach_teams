53dadeec9104d204f53ac6b5b3d0171c7645e509
Mikhail Belousov <tiger.mananticot@yandex.ru>
2026-02-09 22:35:03 +0300

    hideable sidebar on mobile phones and visuals improvements

--- character.js
+++ character.js
@@ -1,42 +1,57 @@
-function rarityToString(character) {
-    switch (character.rarity) {
+function getCharacterById(characterId) {
+  const character = charactersData.characters.find(
+    (character) => character.id === characterId
+  );
+
+  if (!character) {
+    console.log(charactersData.characters);
+    throw new Error(`Character with id '${characterId}' not found`);
+  }
+
+  return character;
+}
+
+class Character {
+  constructor(id, name, img, rarity, classType, baseStats, bonds) {
+    this.id = id;
+    this.name = name;
+    this.img = img;
+    this.rarity = rarity;
+    this.classType = classType;
+    this.baseStats = baseStats;
+    this.bonds = bonds;
+  }
+
+  maxBonds() {
+    switch (this.rarity) {
+      case "SSRFlourite+":
+      case "SSRFlourite":
+      case "SSRCrystal":
+        return 8;
       case "SSRLim":
-        return "SSR";
+        return 7;
       case "SSR":
-        return "SSR";
+        return 6;
+      case "SR":
+        return 3;
+      case "R":
+        return 2;
+      default:
+        throw new Error("Invalid rarity");
+    }
+  }
+  getImagePath() {
+    return "images/characters/" + this.img;
+  }
+
+  rarityToString() {
+    switch (this.rarity) {
       case "SR":
         return "SR";
       case "R":
         return "R";
+      default:
+        return "SSR";
     }
   }
-  
-  function getCharacterById(characterId) {
-    const character = charactersData.characters.find(
-      character => character.id === characterId
-    );
-  
-    if (!character) {
-      console.log(charactersData.characters);
-      throw new Error(`Character with id '${characterId}' not found`);
-    }
-  
-    return character;
-  }
-  
-  function getCharacterImagePath(character) {
-    return "images/characters/" + character.img;
-  }
-  
-  
-  class character {
-    constructor(id, name, img, rarity, classType, baseStats, bonds) {
-      this.id = id;
-      this.name = name;
-      this.img = img;
-      this.rarity = rarity;
-      this.classType = classType;
-      this.baseStats = baseStats;
-      this.bonds = bonds;
-    }
-  }
+}
